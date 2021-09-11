1670d685a28e9465b788b19065f2a700c8e7f097
aameriftekhar@gmail.com <aameriftekhar@gmail.com>
2021-09-11 09:36:18 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = ""
-    secret_key = ""
+    access_key = "AKIAZOCAGBTZR2IIOG5J"
+    secret_key = "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"
 }
 
 resource "aws_ecs_cluster" "web-cluster" {
