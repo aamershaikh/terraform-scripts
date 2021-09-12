60bfa0d63dcac619f06dc336b83fbc64e901bd00
aameriftekhar@gmail.com <aameriftekhar@gmail.com>
2021-09-12 11:08:45 +0530

    testcluster

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_cluster" "web-cluster" {
-  name               = var.cluster_name
+  name               = "myecscluster"
   tags = {
     "env"       = "dev"
     "createdBy" = "aamershaikh"
